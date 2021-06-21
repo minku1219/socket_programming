02aeb44c7dd6198bb0a38f84c1b7dcc5b85adb29
Mayank <68742521+minku1219@users.noreply.github.com>
2021-06-21 20:19:12 +0530

    Only Sender Files

---
+++ sender.py
@@ -0,0 +1,13 @@
+import socket
+
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+recv_add=("127.0.0.1",9999)
+
+msg=input("Enter Your Message: ")
+
+converted_msg=msg.encode('ascii')
+
+s.sendto(converted_msg,recv_add)
+
+print("Your Message has been sent")
